30690cd49cfcd33b8d7bc8f3214f67a2a5e82213
JaviMx <javimxmine@hotmail.com>
2024-11-06 20:01:56 -0600

    Upload proyect

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Resend } from "resend";
+import path from "path";
+import fs from "fs";
+import "dotenv/config";
+
+const resend = new Resend(process.env.API_KEY);
+
+const sendEmail = async () => {
+  try {
+    const templatePath = path.join(__dirname, "/assets/emailTemplate.html");
+    const template = fs.readFileSync(templatePath, "utf-8");
+
+    const response = await resend.emails.send({
+      from: "Test <javi@javimx.com>",
+      to: [`${process.env.EMAIL}`],
+      subject: "Template - Javier Juárez",
+      html: template,
+    });
+
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+sendEmail();
